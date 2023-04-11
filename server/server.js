78613646fe889a8e018efaf4b41b47a176bbdbea
MDrevon <MsDrevon@gmail.com>
2023-04-11 16:28:09 -0500

    undid previous change

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
   res.send("Test");
 });
 
-app.get("api/consoles", (req, res) => {
+app.get("/api/consoles", (req, res) => {
   db.query("SELECT * FROM consoles ORDER BY consoleName ASC", (err, result) => {
     if (err) {
       console.log(err);
@@ -33,7 +33,7 @@ app.get("api/consoles", (req, res) => {
   });
 });
 
-app.get("api/consoles/:id", (req, res) => {
+app.get("/api/consoles/:id", (req, res) => {
   const id = req.params.id;
   db.query("SELECT * FROM consoles WHERE id = $1", [id], (err, result) => {
     if (err) {
@@ -43,7 +43,7 @@ app.get("api/consoles/:id", (req, res) => {
   });
 });
 
-app.get("api/games", (req, res) => {
+app.get("/api/games", (req, res) => {
   db.query(
     "SELECT games.id, games.gamename, genres.genrename, consoles.consolename FROM games JOIN genres ON genres.id = games.genreid JOIN consoles ON consoles.id = games.consoleid ORDER BY gamename ASC",
     (err, result) => {
@@ -55,7 +55,7 @@ app.get("api/games", (req, res) => {
   );
 });
 
-app.get("api/games/console/:consoleId", (req, res) => {
+app.get("/api/games/console/:consoleId", (req, res) => {
   const consoleId = req.params.consoleId;
   db.query(
     "SELECT games.id, games.gamename, genres.genrename, consoles.consolename FROM games JOIN genres ON genres.id = games.genreid JOIN consoles ON consoles.id = games.consoleid WHERE games.consoleId = $1 ORDER BY gamename ASC",
@@ -69,7 +69,7 @@ app.get("api/games/console/:consoleId", (req, res) => {
   );
 });
 
-app.get("api/games/genre/:genreId", (req, res) => {
+app.get("/api/games/genre/:genreId", (req, res) => {
   const genreId = req.params.genreId;
   db.query(
     "SELECT * FROM games WHERE genreId = $1",
@@ -83,7 +83,7 @@ app.get("api/games/genre/:genreId", (req, res) => {
   );
 });
 
-app.get("api/games/:id", (req, res) => {
+app.get("/api/games/:id", (req, res) => {
   const id = req.params.id;
   db.query("SELECT * FROM games WHERE id = $1", [id], (err, result) => {
     if (err) {
@@ -93,7 +93,7 @@ app.get("api/games/:id", (req, res) => {
   });
 });
 
-app.get("api/games/name/:name", (req, res) => {
+app.get("/api/games/name/:name", (req, res) => {
   const name = req.params.name;
   db.query(
     "SELECT * FROM games WHERE gameName LIKE $1",
@@ -107,7 +107,7 @@ app.get("api/games/name/:name", (req, res) => {
   );
 });
 
-app.post("api/games", (req, res) => {
+app.post("/api/games", (req, res) => {
   const name = req.body.gameName;
   const genre = req.body.genreId;
   const console = req.body.consoleId;
@@ -127,7 +127,7 @@ app.post("api/games", (req, res) => {
   // }
 });
 
-app.put("api/games/:id", (req, res) => {
+app.put("/api/games/:id", (req, res) => {
   let name = req.body.name;
   let genre = req.body.genre;
   let console = req.body.console;
@@ -144,7 +144,7 @@ app.put("api/games/:id", (req, res) => {
   );
 });
 
-app.delete("api/games/:id", (req, res) => {
+app.delete("/api/games/:id", (req, res) => {
   db.query(
     "DELETE FROM games WHERE id = $1",
     [req.params.id],
@@ -157,7 +157,7 @@ app.delete("api/games/:id", (req, res) => {
   );
 });
 
-app.get("api/genres", (req, res) => {
+app.get("/api/genres", (req, res) => {
   db.query("SELECT * FROM genres", (err, result) => {
     if (err) {
       console.log(err);
